7d7317fc8196da3364668ced3269deafaba14600
Pranav <pranav2000joglekar@gmail.com>
2020-08-02 12:12:20 +0530

    ui: add placeholder to be displayed before captha is opened

--- backend/captcha_pages/questionnaire.php
+++ backend/captcha_pages/questionnaire.php
@@ -2,7 +2,6 @@
 require '../config.php';
 require '../helpers/add_placeholder.php';
 session_start();
-
 ?>
 <!--
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
@@ -15,7 +14,12 @@ session_start();
 <link rel="stylesheet" href=<?php echo $base_url . "css/questionnaire.css"?>>
 <link rel="stylesheet" href=<?php echo $base_url . "css/common.css"?>>
 <script src= <?php echo $base_url ."js/translate.js"?>/>
-<?php put_placeholder()?>
+<?php 
+error_log($_SESSION['is_open']);
+if(isset($_SESSION['is_open']) && $_SESSION['is_open'] == '0') {
+  put_placeholder();
+}
+?>
 <div class="ca-panel-body">
   <form method="post" id="captcha_form">
     <label class='ca-label'><?php print $_SESSION["ins2"]; ?></label>
@@ -37,7 +41,7 @@ session_start();
     <button class='ca-button' id='switch_lang' onclick="changeLanguage(event, 'questionnaire')"><?php print $_SESSION["lang_switch"]; ?></button>
     <div>
       <select name="lang" id="lang" onchange="changeLanguage(event, 'questionnaire')">
-        <option value="en">English</option>
+        <option value="en" >English</option>
         <option value="hi">Hindi</option>
         <option value="gu">Gujarati</option>
         <option value="mr">Marathi</option>
@@ -66,18 +70,28 @@ session_start();
 </div>
 
 <script>
-  var base_url = "<?php echo $base_url; ?>";
+  var base_url = "<?php echo $base_url; ?>"; 
+  var is_open = "<?php echo $_SESSION['is_open']; ?>";
+  var lang = "<?php echo $_SESSION['lang']; ?>";
+  var region = "<?php echo $_SESSION['region']; ?>";
   var elem_width = document.getElementsByClassName("ca-panel-body")[0].getBoundingClientRect()
   elem_width = elem_width.width;
   img_width = elem_width - 20;
   $('.ca-img').attr("src", base_url + "backend/image_operations/questionnaire_image.php?id=0&height=400&width=" + img_width);
   $(document).ready(function() {
-    var body = document.getElementsByClassName('ca-panel-body')[0];('ca-panel-body')[0];
+    var body = document.getElementsByClassName('ca-panel-body')[0];
     console.log(body);
-    body.style.display="none";
+    console.log(is_open);
+    console.log(!is_open);
+    if(is_open == '0') {
+      console.log('hreer');
+      body.style.display="none";
+    }
     var v = document.getElementById("valid");
     var i = document.getElementById("invalid");
     var e = document.getElementById("enter");
+    $("#lang").val(lang);
+    $("#region").val(region);
 
     $('#captcha_form').on('submit', function(event) {
       event.preventDefault();
--- backend/helpers/add_placeholder.php
+++ backend/helpers/add_placeholder.php
@@ -3,7 +3,7 @@ require '../config.php';
 session_start();
 
 function put_placeholder() {
-  echo ("<script src=" . $GLOBALS['base_url'] . "js/togglecaptcha.js/><div class='ca-placeholder-body' onclick='toggleCaptcha()'> Press to view captcha </div>");
+  echo ("<script src=" . $GLOBALS['base_url'] . "js/togglecaptcha.js/><div class='ca-placeholder-body' onclick='toggleCaptcha()'><span id='view-text'> Press to view captcha</span></div>");
   return;
 }
 ?>
