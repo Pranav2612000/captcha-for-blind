edf26f4c15e83c8d1d3b0821b2e42ce8a28de79e
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-05-04 00:54:23 +0530

    feat: allow users to open the captcha using the keyboard

--- backend/helpers/add_placeholder.php
+++ backend/helpers/add_placeholder.php
@@ -5,14 +5,18 @@ session_start();
 function put_placeholder() {
   echo ("
         <script src=" . $GLOBALS['base_url'] . "js/togglecaptcha.js/>
-        <div class='ca-placeholder-body ca-button' onclick='toggleCaptcha(event)'>
-         <span id='cap_open'>" . add_buttons() . "</span>
-          <span id='cap_closed' class='ca-ignore'>
-            <span id='view-text' class='ca-button'>
-                Press to view captcha
+        <form>
+          <button class='main-btn' onclick='toggleCaptcha(event)'>
+          </button>
+          <div class='ca-placeholder-body ca-button' onclick='toggleCaptcha(event)'>
+           <span id='cap_open'>" . add_buttons() . "</span>
+            <span id='cap_closed' class='ca-ignore'>
+              <span id='view-text' class='ca-button'>
+                  Press to view captcha
+              </span>
             </span>
-          </span>
-        </div>
+          </div>
+        </form>
       ");
   return;
 }
