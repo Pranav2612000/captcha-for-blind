38b3af94c81c542dc8fffbfb9de529396e6b3bf7
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-05-03 22:35:11 +0530

    fix: remove event listeners of previous captcha when switching to a new one

--- js/switch_captcha.js
+++ js/switch_captcha.js
@@ -1,6 +1,10 @@
 function switchCaptcha(e, type) {
   e.preventDefault();
   console.log(type);
+  var el = document.querySelector('body'),
+      elClone = el.cloneNode(true);
+
+  el.parentNode.replaceChild(elClone, el);
   //console.log(document.getElementById("region").value)
   $.ajax({
     //url:"../backend/captcha_pages/questionnaire.php",
